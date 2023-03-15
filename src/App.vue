9fc6da01acb50dab2c10f846a2518a93f83f3c78
robinhoc <robertc_es@yahoo.es>
2023-03-15 19:18:47 -0300

    Aplicado bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
-    <nav>
-      <div>
-        <router-link to="/">12956-VUE</router-link>
-        <ul>
-          <li>
-            <router-link to="/">Novo</router-link>
-          </li>
-          <li>
-            <router-link to="/view">Listar</router-link>
-          </li>          
-        </ul>
-      </div>
-    </nav>
+      <!-- Nav bar  -->
+      <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+        <div class="container">
+          <router-link to="/" class="navbar-brand float-left">12956-Vue</router-link>
+          <ul class="nav navbar-nav flex-row float-right">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link px-3">Novo Contato</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/view" class="nav-link">Listar Contatos</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view>
+        
+      </router-view>
+    </div>
   </div>
-
-<div>
-  <router-view></router-view>
-
-</div>
 
 </template>
