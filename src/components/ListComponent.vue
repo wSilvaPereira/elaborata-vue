72dfd432c1d318770398d728e389627c13200c54
robinhoc <robertc_es@yahoo.es>
2023-03-15 21:48:01 -0300

    Listagem pronta

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,14 +1,42 @@
 <template>
     <div>
-        <h1>List</h1>
+        <table id="tblContatos" class="table table-striped table-responsive table-hover">
+            <thead>
+                <th>Nome</th>
+                <th>idade</th>
+            </thead>
+            <tbody id="tblProdutoBody">
+                <tr v-for="contato in contatos" v-bind:key="contato.id">
+                    <td>{{ contato.nome }}</td>
+                    <td>{{ contato.idade }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- <div v-for="contato in contatos" v-bind:key="contato.id">
+            {{ contato.nome }} - {{contato.idade  }}
+        </div> -->
     </div>
 </template>
 
 <script>
 
-export default{
-    data(){
-        return {}
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            contatos: []
+        }
+    },
+    created() {
+        let apiURL = "http://localhost:3000/contatos";
+
+        axios.get(apiURL).then((resp) => {
+            this.contatos = resp.data;
+            // alert(JSON.stringify(resp));
+        }).catch(error => {
+            console.log(error);
+        })
     }
 }
 
