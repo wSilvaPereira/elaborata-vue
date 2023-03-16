750faa1fde6a86076184be14aa485479cf855edd
robinhoc <robertc_es@yahoo.es>
2023-03-15 21:18:15 -0300

    Primeiro POST

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div>
-      <!-- Nav bar  -->
-      <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
-        <div class="container">
-          <router-link to="/" class="navbar-brand float-left">12956-Vue</router-link>
-          <ul class="nav navbar-nav flex-row float-right">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link px-3">Novo Contato</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/view" class="nav-link">Listar Contatos</router-link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      
+    <!-- Nav bar  -->
+    <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <router-link to="/" class="navbar-brand float-left">12956-Vue</router-link>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link px-3">Novo Contato</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/view" class="nav-link">Listar Contatos</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <!-- Router view -->
     <div class="container mt-5">
       <router-view>
-        
+
       </router-view>
     </div>
   </div>
-
 </template>
--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -1,14 +1,54 @@
 <template>
-    <div>
-        <h1>Create</h1>
+    <div class="row justify-content-center container">
+        <div class="col-md-6">
+            <h1>Novo Contato</h1>
+            <form @submit.prevent="handleSubmitForm" class="container">
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" v-model="contato.nome" required>
+                </div>
+                <div class="form-group">
+                    <label for="idade">Idade</label>
+                    <input type="number" class="form-control" v-model="contato.idade" required>
+                </div>
+                <div class="form-group mt-3">
+                    <button type="submit" class="btn btn-success btn-block w-100">Criar</button>
+                </div>
+            </form>
+        </div>
+
     </div>
 </template>
 
 <script>
 
-export default{
-    data(){
-        return {}
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            contato: {
+                id: 0,
+                nome: '',
+                idade: 0,
+            }
+        }
+    },
+    methods: {
+        handleSubmitForm() {
+            let apiURL = "http://localhost:3000/contatos";
+
+            axios.post(apiURL, this.contato).then(() => {
+                this.$router.push("/view");
+                this.contato = {
+                    "nome": '',
+                    "idade": 0
+                }
+                alert('Contato registrado')
+            }).catch(error => {
+                console.log(error);
+            })
+        }
     }
 }
 
